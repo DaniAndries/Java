d47baa4413e9bdb0b3ce4d648fed81109c845ae5
ElRuso78 <daniandries78@gmail.com>
2023-11-02 10:33:22 +0100

    2/11

--- Tema5/src/T5E32.java
+++ Tema5/src/T5E32.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class T5E32 {
     public static void main(String[] args) {
-        Scanner lector = new Scanner(System.in);
         int contador = 0;
         int aleatorio = 0;
         while (aleatorio != 5) {
@@ -12,6 +10,5 @@ public class T5E32 {
             contador = contador + 1;
         }
         System.out.println("Se ha lanzado: " + contador + " veces");
-        lector.close();
     }
 }
