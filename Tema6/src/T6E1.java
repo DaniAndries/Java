5c9b88615a6a4ebd2b63c14541988e62bbddb61b
Daniel Andries <daniandries78@gmail.com>
2023-11-08 12:10:30 +0100

    8/11

--- Tema6/src/T6E1.java
+++ Tema6/src/T6E1.java
@@ -13,12 +13,23 @@ public class T6E1 {
         punto3.x = -3;
         punto3.y = 7;
 
+        System.out.println("");
+        System.out.println("Primera parte: ");
         System.out.println("(" + punto1.x + "," + punto1.y + ")");
         System.out.println("(" + punto2.x + "," + punto2.y + ")");
         System.out.println("(" + punto3.x + "," + punto3.y + ")");
 
-        
+        punto1.x = 2 + 5;
+        punto1.y = 5 * 8 + 0;
+        punto2.x = 7 / 10;
+        punto2.y = 90 * 10;
+        punto3.x = 8 / -3;
+        punto3.y = 4 - 7;
 
-
+        System.out.println("");
+        System.out.println("Segunda parte: ");
+        System.out.println("(" + punto1.x + "," + punto1.y + ")");
+        System.out.println("(" + punto2.x + "," + punto2.y + ")");
+        System.out.println("(" + punto3.x + "," + punto3.y + ")");
     }
 }
