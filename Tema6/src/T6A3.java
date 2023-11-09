c74c40ea4a4f0ba1aac4b49e239864cd707050aa
Daniel Andries <daniandries78@gmail.com>
2023-11-09 14:56:08 +0100

    9/11

--- Tema6/src/T6A3.java
+++ Tema6/src/T6A3.java
@@ -14,16 +14,24 @@ public class T6A3 {
         rectangulo1.y1 = 0;
         rectangulo1.x2 = 5;
         rectangulo1.y2 = 5;
-        double lado1 = rectangulo1.x1 + rectangulo1.y1;
-        double area1 = rectangulo1.x2 + rectangulo1.y2;
+
+        double lado1 = rectangulo1.x1 - rectangulo1.x2;
+        double altura1 = rectangulo1.y1 - rectangulo1.y2;
+        double lados1 = lado1 + lado1 + altura1 + altura1;
+        double perimetro1=altura1*lado1;
 
         rectangulo2.x1 = 7;
         rectangulo2.y1 = 9;
         rectangulo2.x2 = 2;
         rectangulo2.y2 = 3;
 
-        System.out.println("Rectángulo 1. Lados: " + lado1 + " área: " + area1);
-        System.out.println("Rectángulo 2. Lados: " + rectangulo2.x1 + rectangulo2.y1 + " área: " + rectangulo2.x2 * rectangulo2.y2);
+        double lado2 = rectangulo2.x1 - rectangulo2.x2;
+        double altura2 = rectangulo2.y1 - rectangulo2.y2;
+        double lados2 = lado2 + lado2 + altura2 + altura2;
+        double perimetro2 = altura2*lado2;
+
+        System.out.println("Rectángulo 1. Lados: " + lados1 + " área: " + perimetro1);
+        System.out.println("Rectángulo 2. Lados: " + lados2 + " área: " + perimetro2);
 
     }
 }
