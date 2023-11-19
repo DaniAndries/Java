d53a3585207994f654d4c9c6ea54b0d034be48ad
ElRuso78 <daniandries78@gmail.com>
2023-11-19 20:19:23 +0100

    19/11

--- Tema6/src/T6E/T6E4.java
+++ Tema6/src/T6E/T6E4.java
@@ -6,19 +6,34 @@ class Articulo {
     private String nombre;
     private double precio;
     private static final int iva = 21;
+    private static final int ivaReducido = 10;
+    private static final int ivaSuperReducido = 4;
     private int cuantosQuedan;
+    private final int tipoIVA;
 
-    public Articulo(String nombre, double precio, int cuantosQuedan){
+    public Articulo(String nombre, double precio, int cuantosQuedan, int tipoIVA) {
         this.nombre = nombre;
         if (precio < 0) System.err.println("Parametro inválido");
         else this.precio = precio;
         if (cuantosQuedan < 0) System.err.println("Parametro inválido");
         this.cuantosQuedan = cuantosQuedan;
+        if (tipoIVA == 1) this.tipoIVA = iva;
+        else if (tipoIVA == 2) this.tipoIVA = ivaReducido;
+        else if (tipoIVA == 3) this.tipoIVA = ivaSuperReducido;
+        else {
+            System.err.println("Parametro inválido");
+            this.tipoIVA = iva;
+        }
     }
 
-    public int getIVA(){
-        return iva;
+    public int getTipoIVA() {
+        if (this.tipoIVA == 1) return iva;
+
+        else if (this.tipoIVA == 2) return ivaReducido;
+
+        return ivaSuperReducido;
     }
+
     public String getNombre() {
         return nombre;
     }
@@ -36,48 +51,49 @@ class Articulo {
     }
 
     public void setPrecio(double precio) {
-        if(precio<0){
+        if (precio < 0) {
             this.precio = 0;
             System.err.println("Parámetro inválido");
-        }
-        else this.precio = precio;
+        } else this.precio = precio;
     }
 
 
     public void setCuantosQuedan(int cuantosQuedan) {
-        if(cuantosQuedan < 0){
+        if (cuantosQuedan < 0) {
             this.cuantosQuedan = 0;
             System.err.println("Parámetro inválido");
-        }
-        else this.cuantosQuedan = cuantosQuedan;
+        } else this.cuantosQuedan = cuantosQuedan;
     }
 
     /**
      * Calcula el precio una vez añadido el IVA
+     *
      * @return Devuelve el precio una vez aplicado el IVA
      */
-    public double getPVP(){
-        double iva = this.precio * ((double) this.getIVA() / 100);
+    public double getPVP() {
+        double iva = this.precio * ((double) this.tipoIVA / 100);
         return (this.precio + iva);
     }
 
     /**
      * Calcula y aplica un descuento a unos productos
+     *
      * @param descuento es la cantidad del descuento a aplicar
      * @return Devuelve el precio una vez aplicado el descuento
      */
-    public double getPVPDescuento(int descuento){
-        return (this.precio-(this.precio * ((double) descuento/ 100)));
+    public double getPVPDescuento(int descuento) {
+        return (this.precio - (this.precio * ((double) descuento / 100)));
     }
 
     /**
      * Resta una cantidad al inventario
+     *
      * @param vender es la cantidad a restar
      * @return Devuelve un boolean dependiendo si se puede o no restar esa cantidad
      */
-    public boolean vender(int vender){
+    public boolean vender(int vender) {
         if (this.cuantosQuedan - vender < 0) return false;
-        else{
+        else {
             this.cuantosQuedan = this.cuantosQuedan - vender;
             return true;
         }
@@ -85,27 +101,28 @@ class Articulo {
 
     /**
      * Añade más cantidad al inventario
+     *
      * @param almacenar la cantidad a añadir
      * @return Devuelve true or false si se puede o no añadir más al inventario
      */
-    public boolean almacenar(int almacenar){
+    public boolean almacenar(int almacenar) {
         if (this.cuantosQuedan + almacenar < 0) return false;
-        else{
+        else {
             this.cuantosQuedan = this.cuantosQuedan + almacenar;
             return true;
         }
     }
 
-    public void imprimir(){
-        System.out.println(this.nombre + " - Precio:" + this.precio + " - IVA:" + this.getIVA() + " - PVP:" + this.getPVP() + " - Quedan:" + this.cuantosQuedan);
+    public void imprimir() {
+        System.out.println(this.nombre + " - Precio:" + this.precio + " - IVA:" + this.getTipoIVA() + " - PVP:" + this.getPVP() + " - Quedan:" + this.cuantosQuedan);
     }
 }
 
 public class T6E4 {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        Articulo articulo = new Articulo(lector.nextLine(), lector.nextDouble(), lector.nextInt());
-        Articulo articulo2 = new Articulo(lector.nextLine(), lector.nextDouble(), lector.nextInt());
+        Articulo articulo = new Articulo(lector.nextLine(), lector.nextDouble(), lector.nextInt(), lector.nextInt());
+        Articulo articulo2 = new Articulo(lector.nextLine(), lector.nextDouble(), lector.nextInt(), lector.nextInt());
 
 
         articulo.imprimir();
@@ -117,7 +134,6 @@ public class T6E4 {
 
         articulo.imprimir();
         articulo2.imprimir();
-
 
 
     }
