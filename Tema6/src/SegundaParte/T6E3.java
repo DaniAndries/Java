9be515496de240efbc0543c2081de723a7fafc3f
ElRuso78 <daniandries78@gmail.com>
2023-11-21 19:10:47 +0100

    21/11

--- Tema6/src/SegundaParte/T6E3.java
+++ Tema6/src/SegundaParte/T6E3.java
@@ -1,12 +1,59 @@
 package SegundaParte;
 
-class bombillas{
+class Bombillas {
+    private boolean interruptor;
+    private static boolean interruptorGeneral = true;
 
+    public Bombillas(boolean interruptor) {
+        this.interruptor = interruptor;
+    }
 
+    public boolean getInterruptor() {
+        return interruptor;
+    }
 
+    public static boolean getInterruptorGeneral() {
+        return interruptorGeneral;
+    }
+
+    public void setInterruptor(boolean interruptor) {
+        this.interruptor = interruptor;
+    }
+
+    public static void setInterruptorGeneral(boolean interruptorGeneral) {
+        Bombillas.interruptorGeneral = interruptorGeneral;
+    }
+
+    public void imprimir() {
+        if (!this.interruptor || !interruptorGeneral)
+            System.out.println("El interruptor está apagado por lo que no da luz");
+        else System.out.println("los interruptores está encendido por lo que da luz");
+    }
 }
+
 public class T6E3 {
     public static void main(String[] args) {
+        Bombillas salon = new Bombillas(false);
+        Bombillas cocina = new Bombillas(false);
 
+        salon.imprimir();
+        cocina.imprimir();
+
+        salon.setInterruptor(true);
+        cocina.setInterruptor(true);
+
+        salon.imprimir();
+        cocina.imprimir();
+
+        Bombillas.setInterruptorGeneral(false);
+
+        salon.imprimir();
+        cocina.imprimir();
+
+        System.out.println("Estado de los interruptores:");
+
+        System.out.println("Interruptor del salon " + salon.getInterruptor());
+        System.out.println("Interruptor de la cocina " + cocina.getInterruptor());
+        System.out.println("Interruptor general " + Bombillas.getInterruptorGeneral());
     }
 }
