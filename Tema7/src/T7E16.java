f48309d6854e46a14aefc6763926a6d426f3c55d
Daniel Andries <daniandries78@gmail.com>
2023-12-13 13:21:34 +0100

    13/12

--- Tema7/src/T7E16.java
+++ Tema7/src/T7E16.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class T7E16 {
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que introduzca un número decimal
+        System.out.print("Introduce un número decimal: ");
+        int decimalNumber = scanner.nextInt();
+
+        // Obtener la representación hexadecimal utilizando la función personalizada
+        String hexadecimalCustom = decimalToHexadecimalCustom(decimalNumber);
+
+        // Obtener la representación hexadecimal utilizando las clases de Java
+        String hexadecimalJava = Integer.toHexString(decimalNumber).toUpperCase();
+
+        // Mostrar los resultados
+        System.out.println("Resultado utilizando la función personalizada: " + hexadecimalCustom);
+        System.out.println("Resultado utilizando las clases de Java: " + hexadecimalJava);
+
+        // Comparar los resultados
+        if (hexadecimalCustom.equals(hexadecimalJava)) {
+            System.out.println("Los resultados son iguales.");
+        } else {
+            System.out.println("Los resultados son diferentes.");
+        }
+    }
+
+    // Función para convertir decimal a hexadecimal (sin utilizar las clases de Java)
+    private static String decimalToHexadecimalCustom(int decimalNumber) {
+        StringBuilder hexadecimal = new StringBuilder();
+
+        while (decimalNumber > 0) {
+            int remainder = decimalNumber % 16;
+            char hexDigit = (char) (remainder < 10 ? remainder + '0' : remainder - 10 + 'A');
+            hexadecimal.insert(0, hexDigit);
+            decimalNumber /= 16;
+        }
+
+        return hexadecimal.toString();
     }
 }
