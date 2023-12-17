9c788e9855191691bf966bb3d977710a02cd1ab9
ElRuso78 <daniandries78@gmail.com>
2023-12-17 15:27:05 +0100

    17/12

--- Tema7/src/T7E38.java
+++ Tema7/src/T7E38.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class T7E38 {
-    public static String contraseña (int numeroCaracteres, int nivelSeguridad){
+    public static String password (int numeroCaracteres, int nivelSeguridad){
         // Restricciones
         if (numeroCaracteres < 8) {
             numeroCaracteres = 8;
@@ -20,34 +20,26 @@ public class T7E38 {
         /**
          * Concatena las cadenas de carácteres permitidos dependiendo del nivel de seguridad
          */
-        String caracteresPermitidos = "";
-        switch (nivelSeguridad) {
-            case 1:
-                caracteresPermitidos = letrasMinusculas;
-                break;
-            case 2:
-                caracteresPermitidos = letrasMinusculas + letrasMayusculas;
-                break;
-            case 3:
-                caracteresPermitidos = letrasMinusculas + letrasMayusculas + numeros;
-                break;
-            case 4:
-                caracteresPermitidos = letrasMinusculas + letrasMayusculas + numeros + simbolos;
-                break;
-        }
+        String caracteresPermitidos = switch (nivelSeguridad) {
+            case 1 -> letrasMinusculas;
+            case 2 -> letrasMinusculas + letrasMayusculas;
+            case 3 -> letrasMinusculas + letrasMayusculas + numeros;
+            case 4 -> letrasMinusculas + letrasMayusculas + numeros + simbolos;
+            default -> "";
+        };
 
         /**
-         * Genera la contraseña basandose en la longitud y en los carácteres permitidos generando un número aleatorio
+         * Genera la contraseña basándose en la longitud y en los carácteres permitidos generando un número aleatorio
          * Crea un número aleatorio y selecciona el carácter específico de la cadena caracteresPermitidos
          * Después lo añade a contraseñaGenerada
          */
-        StringBuilder contraseñaGenerada = new StringBuilder();
+        StringBuilder passwordGenerada = new StringBuilder();
         for (int i = 0; i < numeroCaracteres; i++) {
             int random = ThreadLocalRandom.current().nextInt(0, caracteresPermitidos.length());
-            contraseñaGenerada.append(caracteresPermitidos.charAt(random));
+            passwordGenerada.append(caracteresPermitidos.charAt(random));
         }
 
-        return contraseñaGenerada.toString();
+        return passwordGenerada.toString();
     }
     
     public static void main(String[] args) {
@@ -57,7 +49,7 @@ public class T7E38 {
         System.out.println("Indica el nivel de seguridad de la contraseña: ");
         int nivelSeguridad = lector.nextInt();
 
-        String password = contraseña(numeroCaracteres, nivelSeguridad);
+        String password = password(numeroCaracteres, nivelSeguridad);
 
         System.out.println("Tu contraseña es: " + password);
     }
