8800b881bf6587b221f047dac106d52e1014747d
ElRuso78 <daniandries78@gmail.com>
2023-12-23 16:19:29 +0100

    23/12

---
+++ Tema7/src/Extra2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Extra2 {
+    /**
+     *
+     *     Desarrolla un programa en Java que solicite al usuario ingresar una cadena que represente un número de teléfono
+     *     en el formato internacional. Utiliza el método .matches para verificar si la cadena cumple con las siguientes condiciones:
+     *     Debe comenzar con el símbolo "+" seguido de un código de país de 1 a 3 dígitos.
+     *     Después del código de país, debe haber un número de teléfono que consta de al menos 8 dígitos.
+     *     Entre los dígitos del número de teléfono, puede haber espacios en blanco, guiones (-) o puntos (.).
+     *
+     * Imprime un mensaje indicando si la cadena representa un número de teléfono válido según estos criterios.
+     */
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        String telefono = lector.nextLine();
+
+    }
+}
